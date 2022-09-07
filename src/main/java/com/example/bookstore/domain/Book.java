da7fe0b8c20530fae5c3f021c04f5df2fdd740f3
MatthieuBruh <matthieu.bruhwiler@etu.hesge.ch>
2022-09-07 11:50:22 +0300

    First commit

---
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -0,0 +1,86 @@
+package com.example.bookstore.domain;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+    private String isbn;
+    private double price;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, int year, String isbn, double price) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                ", isbn='" + isbn + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
