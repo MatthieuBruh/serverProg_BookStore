87e8a19cf12a6eddddfdc1f2d66ab1d9e9aabcb9
MatthieuBruh <matthieu.bruhwiler@etu.hesge.ch>
2022-09-12 16:47:39 +0300

    Booklist added

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,17 +1,22 @@
 package com.example.bookstore.web;
 
+import com.example.bookstore.domain.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 public class BookController {
+    @Autowired
+    private BookRepository repository;
 
-    /* @RequestMapping(value = "/index", method = RequestMethod.GET)
-    private String index() {
-        return "Welcome to the new Book store";
+    @RequestMapping(value = "/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        System.out.println(repository.findAll());
+        return "bookList";
     }
-    */
 }
